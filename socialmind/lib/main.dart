8ae2400765495d404a1a29023e48ada57615c785
Lance <austinkarki84@gmail.com>
2024-03-26 04:03:54 +0545

    added webapp

--- socialmind/lib/main.dart
+++ socialmind/lib/main.dart
@@ -1,7 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:socialmind/Frontend/StartPage.dart';
+import 'shared/constants.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: FirebaseOptions(
+            apiKey: Constants.apiKey,
+            appId: Constants.appId,
+            messagingSenderId: Constants.messageSenderId,
+            projectId: Constants.projectId));
+  }
   runApp(const MyApp());
 }
 
---
+++ socialmind/lib/shared/constants.dart
@@ -0,0 +1,6 @@
+class Constants {
+  static String apiKey = "AIzaSyAPBFVJ7MmU06JUdnhSBF5IIqkjIPNjlgA";
+  static String appId = "1:221513340811:web:2c7b8f04b803f91abf418e";
+  static String messageSenderId = "221513340811";
+  static String projectId = "socialminds-34a74";
+}
