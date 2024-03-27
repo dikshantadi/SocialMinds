4452e503a1bcb9d4115c2724b49a5178403d5400
Lance <austinkarki84@gmail.com>
2024-03-27 23:52:55 +0545

    authentication incomplete part 2

--- socialmind/lib/backend/authentication.dart
+++ socialmind/lib/backend/authentication.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'database.dart';
 
-class Auth {
+class Authentication {
   final firebaseAuth = FirebaseAuth.instance;
   Future registerWithEmail(
       String userName, String email, String password) async {
@@ -8,6 +9,7 @@ class Auth {
       User user = (await firebaseAuth.createUserWithEmailAndPassword(
               email: email, password: password))
           .user!;
+      await Database(uid: user.uid).createUserDocument(userName, email);
     } on FirebaseAuthException catch (e) {
       return e.message;
     }
--- socialmind/lib/backend/database.dart
+++ socialmind/lib/backend/database.dart
@@ -3,4 +3,13 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class Database {
   final String? uid;
   Database({this.uid});
+  final CollectionReference userCollection =
+      FirebaseFirestore.instance.collection("Users");
+
+  Future createUserDocument(String userName, email) async {
+    await userCollection.doc(uid).set({
+      'userName': userName,
+      'email': email,
+    });
+  }
 }
