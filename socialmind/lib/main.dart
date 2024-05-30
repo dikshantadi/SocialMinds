623814cf182564345a819380e403d3333b60da8c
Dikshyant Adhikari <adhikarybad@gmail.com>
2024-05-30 21:06:13 +0545

    UI

--- socialmind/lib/main.dart
+++ socialmind/lib/main.dart
@@ -19,18 +19,18 @@ Future<void> main() async {
   runApp(const MyApp());
 }
 
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
+/*class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return const MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       home: StartPage(),
-//     );
-//   }
-// }
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: StartPage(),
+    );
+  }
+}*/
 
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
